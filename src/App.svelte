0dc71eb0ae33f6eaf3c9654c215f862256401390
butlermathandscienceclub <80225040+butlermathandscienceclub@users.noreply.github.com>
2022-02-08 18:35:15 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,8 @@ function go(){
 	var i = document.forms["in"]["in1"].value
 	document.getElementById("if").src = `https://s-api.kellanb.repl.co/?search=${i}`;
 		document.getElementById("if").style.display = "block"
+
+		
 document.getElementsByClassName("noif")[0].style.display = "none"
 }
 </script>
@@ -10,18 +12,21 @@ document.getElementsByClassName("noif")[0].style.display = "none"
 <div class="noif">
 	<h1>super search!</h1>
 	<br>
-	
+	<p> press search. Please don't press enter</p>
 	<form name="in">
 		<input name="in1" placeholder="search the web!" id="in1" > 
 	</form>
+	<br>
 	<button on:click={go} id="b1">search!</button>
 </div>
 <iframe style="display:none" id="if"> </iframe>	
+<footer>
+<p>Made by <a href="http://bmsc.ga/" target="_blank">Butler Math and Science Club</a></p>
+</footer>
 </main>
 <style>
 main{
 		display:block; 
-
 	text-align:center;
 }
 iframe{
@@ -40,13 +45,15 @@ iframe{
 #b1:hover{
 	color:white;
 	background-color:black;
-
 }
 #in1:focus{
 		color:white;
 	background-color:black;
-
 }
-
-
+footer {
+   position:absolute;
+   bottom:0;
+   width:100%;
+   height:60px;
+}
 </style>
