d1431a35485768ae485ca6e1092fe8bb31f7c8f4
butlermathandscienceclub <80225040+butlermathandscienceclub@users.noreply.github.com>
2022-02-10 19:18:49 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,24 @@ function go(){
 
 		
 document.getElementsByClassName("noif")[0].style.display = "none"
+document.getElementById("foot").style.display = "none"
 }
+window.isd = 0;
+function dark(){
+if(window.isd==0)
+{document.querySelector(":root").style.color = "white"
+document.querySelector(":root").style.backgroundColor = "black"
+window.isd = 1;
+
+}
+else if(window.isd==1){
+document.querySelector(":root").style.color = "black"
+document.querySelector(":root").style.backgroundColor = "white"
+window.isd = 0;
+}
+
+}
+
 </script>
 <main>
 <div class="noif">
@@ -17,20 +34,27 @@ document.getElementsByClassName("noif")[0].style.display = "none"
 		<input name="in1" placeholder="search the web!" id="in1" > 
 	</form>
 	<br>
-	<button on:click={go} id="b1">search!</button>
+	<button on:click={go} id="b1">search! </button>
 </div>
 <iframe style="display:none" id="if"> </iframe>	
-<footer>
+<footer id="foot"> 
 <p>Made by <a href="http://bmsc.ga/" target="_blank">Butler Math and Science Club</a></p>
+<button on:click={dark}>toggle dark</button>
+
+
 </footer>
 </main>
 <style>
+	:root{
+		--se :  "🔎"
+	}
 main{
 		display:block; 
+
 	text-align:center;
 }
 iframe{
-	    position: fixed;
+    position: fixed;
     top: 0px;
     bottom: 0px;
     right: 0px;
@@ -45,15 +69,31 @@ iframe{
 #b1:hover{
 	color:white;
 	background-color:black;
+
 }
 #in1:focus{
 		color:white;
 	background-color:black;
+
 }
 footer {
    position:absolute;
-   bottom:0;
+   bottom:50px;
    width:100%;
    height:60px;
 }
+#in1{
+	  width: 50%;
+		height:30px;
+		  border-radius: 25px;
+	padding:12px 20px
+}
+#b1{
+	width:90px;
+	height:40px;
+}
+#b1::after{
+	content: var(--se)
+}
+
 </style>
